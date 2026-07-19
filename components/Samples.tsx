a6a877365de9b73ce4e516bde953decc42fb251f
intradayray-maker <intradayray@gmail.com>
2026-07-18 22:38:23 -0400

    gfhvjbkn

--- components/Samples.tsx
+++ components/Samples.tsx
@@ -7,9 +7,9 @@ export default function Samples() {
       <div className="max-w-4xl mx-auto rounded-xl overflow-hidden border border-border bg-card shadow-glow">
         <div className="aspect-video bg-black">
           <iframe
-            src="https://www.youtube-nocookie.com/embed/pgdQwXzB4h8?controls=1&modestbranding=1&playsinline=1"
+            src="https://player.vimeo.com/video/1211112648?autoplay=0&loop=0&muted=0&controls=1&title=0&byline=0&portrait=0&dnt=1"
             className="w-full h-full"
-            allow="autoplay; encrypted-media"
+            allow="autoplay; encrypted-media; fullscreen; picture-in-picture"
           />
         </div>
       </div>
