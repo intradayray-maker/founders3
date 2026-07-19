da103af00c6d4ab382c4f8fe242fa0c0c8c2fd95
intradayray-maker <intradayray@gmail.com>
2026-07-18 22:25:59 -0400

    asdfasdfdf

--- components/Samples.tsx
+++ components/Samples.tsx
@@ -4,11 +4,10 @@ export default function Samples() {
   return (
     <section id="samples" className="py-1 px-6 text-center">
 
-
       <div className="max-w-4xl mx-auto rounded-xl overflow-hidden border border-border bg-card shadow-glow">
         <div className="aspect-video bg-black">
           <iframe
-            src="https://www.youtube-nocookie.com/embed/VQrnM931Ldo?controls=1&modestbranding=1&playsinline=1"
+            src="https://www.youtube-nocookie.com/embed/pgdQwXzB4h8?controls=1&modestbranding=1&playsinline=1"
             className="w-full h-full"
             allow="autoplay; encrypted-media"
           />
